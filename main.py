0199a0f08e3faef26a19b8b34004c1f665d754dc
Screenbones <screenbones.core@gmail.com>
2022-12-09 21:28:18 -0700

    Store user input.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 def main():
     USER_PROMPT = "Enter a todo: "
-    user_text = input(USER_PROMPT)
-    print(user_text)
+    todo1 = input(USER_PROMPT)
+    todo2 = input(USER_PROMPT)
+    todo3 = input(USER_PROMPT)
+
+    todos = [todo1, todo2, todo3, "Hello"]
+    print(todos)
+
+    print(type(todo1))
 
 if __name__ == '__main__':
     main()
