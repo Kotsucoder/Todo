6307877b45df52439f36b008a3e57f008244b5ef
Screenbones <whoami@kotsu.red>
2024-03-02 23:17:21 -0700

    Bug-Fixing Exercises plus additional from Udemy

---
+++ Udemy Exercises/exercise3.py
@@ -0,0 +1,6 @@
+# This coding exercise could not be done on Udemy as it apparently doesn't support while loops.
+
+name = input("Enter your name: ")
+
+while True:
+    print(name.capitalize())
---
+++ Udemy Exercises/exercise4.py
@@ -0,0 +1,5 @@
+# This coding exercise could not be done on Udemy as it apparently doesn't support while loops.
+
+while True:
+    name = input("What is your name? ")
+    print(name.capitalize())
