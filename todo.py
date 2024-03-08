d81087b8cd29f759a7d24e3bce1f6211ea8bddfe
Screenbones <whoami@kotsu.red>
2024-03-08 16:24:25 -0700

    Editing and Completing Todo Items with-context-manager

--- todo.py
+++ todo.py
@@ -6,13 +6,16 @@ while True:
         case 'add':
             todo = input("Enter a todo: ") + '\n'
 
-            with open('todo.txt', 'r') as file:
+            with open('todos.txt', 'r') as file:
                 todos = file.readlines()
 
             todos.append(todo)
 
             with open('todos.txt', 'w') as file:
                 file.writelines(todos)
+
+
+
         case 'show':
             with open('todos.txt', 'r') as file:
                 todos = file.readlines()
@@ -21,14 +24,42 @@ while True:
                 item = item.strip('\n')
                 row = f'{index + 1}. {item}'
                 print(row)
+
+
+
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
             new_todo = input("Enter new todo: ")
-            todos[number] = new_todo
+            todos[number] = new_todo + '\n'
+            
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
+
+
         case 'complete':
             number = int(input("Number of the todo to complete: "))
-            todos.pop(number - 1)
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
+            message = f'Todo {todo_to_remove} was removed from the list.'
+            print(message)
+
+
+
         case 'exit':
             break
 
