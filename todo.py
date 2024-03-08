1ef73b296a05d6cf38bd865ae9a308aa9509939b
Screenbones <whoami@kotsu.red>
2024-03-08 16:14:10 -0700

    Optimizing the Code with-context-manager

--- todo.py
+++ todo.py
@@ -6,34 +6,19 @@ while True:
         case 'add':
             todo = input("Enter a todo: ") + '\n'
 
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            with open('todo.txt', 'r') as file:
+                todos = file.readlines()
 
             todos.append(todo)
 
-            file = open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
         case 'show':
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
-
-            #----------------------------------
-            # Method One:
-            # new_todos = []
-
-            # for item in todos:
-            #     new_item = item.strip('\n')
-            #     new_todos.append(new_item)
-            #----------------------------------
-            # Method Two:
-            # new_todos = [item.strip('\n') for item in todos]
-            #----------------------------------
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
             
             for index, item in enumerate(todos):
-                item = item.strip('\n') # Implimented Method
+                item = item.strip('\n')
                 row = f'{index + 1}. {item}'
                 print(row)
         case 'edit':
