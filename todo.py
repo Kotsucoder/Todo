06c2285b2e902a1786cc0b8382b662ed2857279a
Screenbones <whoami@kotsu.red>
2024-03-10 07:32:49 -0700

    Anticipating Program Errors try-except continue

--- todo.py
+++ todo.py
@@ -27,35 +27,49 @@ while True:
 
 
     elif user_action.startswith('edit'):
-        number = int(user_action[5:])
-        number = number - 1
+        try:
+            number = int(user_action[5:])
+            number = number - 1
 
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
-        new_todo = input("Enter new todo: ")
-        todos[number] = new_todo + '\n'
-        
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo + '\n'
+            
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+        except IndexError:
+            print('Invalid Command: There is no todo with that number.')
+            continue
+        except ValueError:
+            print('Invalid Command: Expected number for todo to edit.')
+            continue
 
 
 
     elif user_action.startswith('complete'):
-        number = int(user_action[9:])
+        try:
+            number = int(user_action[9:])
 
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
-        index = number - 1
-        todo_to_remove = todos[index].strip('\n')
-        todos.pop(index)
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
 
-        message = f'Todo {todo_to_remove} was removed from the list.'
-        print(message)
+            message = f'Todo {todo_to_remove} was removed from the list.'
+            print(message)
+        except IndexError:
+            print('Invalid Command: There is no todo with that number.')
+            continue
+        except ValueError:
+            print('Invalid Command: Expected number for todo to edit.')
+            continue
 
 
 
