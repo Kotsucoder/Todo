ff20944ce17bd37c69120593cee134beed6dc33a
Screenbones <whoami@kotsu.red>
2024-03-10 07:25:12 -0700

    Fixing Two Bugs in the Program bugs

--- todo.py
+++ todo.py
@@ -2,20 +2,20 @@ while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
-    if 'add' in user_action:
+    if user_action.startswith('add'):
         todo = user_action[4:]
 
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
 
-        todos.append(todo)
+        todos.append(todo + '\n')
 
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
 
 
 
-    elif 'show' in user_action:
+    elif user_action.startswith('show'):
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
         
@@ -26,7 +26,7 @@ while True:
 
 
 
-    elif 'edit' in user_action:
+    elif user_action.startswith('edit'):
         number = int(user_action[5:])
         number = number - 1
 
@@ -41,7 +41,7 @@ while True:
 
 
 
-    elif 'complete' in user_action:
+    elif user_action.startswith('complete'):
         number = int(user_action[9:])
 
         with open('todos.txt', 'r') as file:
@@ -59,7 +59,7 @@ while True:
 
 
 
-    elif 'exit' in user_action:
+    elif user_action.startswith('exit'):
         break
 
 
