fb82f75b86441fd99063bdb2425f3e9589de82cb
Screenbones <whoami@kotsu.red>
2024-03-14 07:13:12 -0700

    Organising the Code in Modules modules import

---
+++ functions.py
@@ -0,0 +1,11 @@
+def get_todos(filepath='todos.txt'):
+    """Read a text file and return the list of todo items."""
+    with open(filepath, 'r') as file:
+        todos = file.readlines()
+    return todos
+
+
+def write_todos(todos, filepath='todos.txt'):
+    """Write the todo items list in a text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos)
--- todo.py
+++ todo.py
@@ -1,15 +1,5 @@
-def get_todos(filepath='todos.txt'):
-    """Read a text file and return the list of todo items."""
-    with open(filepath, 'r') as file:
-        todos = file.readlines()
-    return todos
-
-
-def write_todos(todos, filepath='todos.txt'):
-    """Write the todo items list in a text file."""
-    with open(filepath, 'w') as file:
-        file.writelines(todos)
-
+# from functions import get_todos, write_todos
+import functions
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
@@ -18,16 +8,16 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos.append(todo + '\n')
 
-        write_todos(todos)
+        functions.write_todos(todos)
 
 
 
     elif user_action.startswith('show'):
-        todos = get_todos()
+        todos = functions.get_todos()
         
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -41,12 +31,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
             
-            write_todos(todos)
+            functions.write_todos(todos)
         except IndexError:
             print('Invalid Command: There is no todo with that number.')
             continue
@@ -60,13 +50,13 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
             message = f'Todo {todo_to_remove} was removed from the list.'
             print(message)
