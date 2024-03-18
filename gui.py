667ab049700fae6d56b01872a6f355a47e44272f
Screenbones <whoami@kotsu.red>
2024-03-17 20:49:55 -0700

    Implimenting an "Add Todo" Button gui-buttons

--- gui.py
+++ gui.py
@@ -2,10 +2,24 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input = sg.InputText(tooltip="Enter todo")
+input = sg.InputText(tooltip="Enter todo", key='todo')
 add_button = sg.Button("Add")
 
-window = sg.Window('KotsuCoder Todo App', layout=[[label], [input, add_button]])
-window.read()
-print("Hello")
+window = sg.Window('KotsuCoder Todo App', 
+                   layout=[[label], [input, add_button]], 
+                   font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
