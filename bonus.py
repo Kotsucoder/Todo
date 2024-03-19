1aaa40f8372a18c6496754eded51faf6a541835c
Screenbones <whoami@kotsu.red>
2024-03-19 13:56:55 -0700

    Coding Exercise

--- bonus.py
+++ bonus.py
@@ -1,16 +1,22 @@
 import streamlit as st
 from PIL import Image
 
+
+def grayscale(image):
+    img = Image.open(image)
+    gray_img = img.convert("L")
+    st.image(gray_img)
+
+
+uploaded_image = st.file_uploader("Upload Image", key="upload")
+
 with st.expander("Start Camera"):
-    # Start the Camera
     camera_image = st.camera_input("Camera")
 
+
 if camera_image:
-    # Create a pillow image instance
-    img = Image.open(camera_image)
+    grayscale(camera_image)
+if uploaded_image:
+    grayscale(st.session_state["upload"])
 
-    # Conbert the pillow image to grayscale
-    gray_img = img.convert("L")
-
-    # Render the grayscale image on the webpage.
-    st.image(gray_img)
+st.session_state
